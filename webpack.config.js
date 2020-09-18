c599e628242e995ee4e7e7c600a90284d2c12d94
desertsniper87 <torshobuet@gmail.com>
2020-09-18 15:38:27 +0600

    test run

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,18 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use : {
-                    loader: "babel-loader"
-                },
-
+                use : "babel-loader"
+            },
+            {
                 test: /\.html$/,
-                use : {
-                    loader: "html-loader"
-                }
+                use : "html-loader"
             }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin ({
             template: "./src/index.html",
-            filename: "index.html",
+            filename: "./index.html",
         })
     ]
 }
